642346643b8b423a3a032497356ed4708671389c
trrenty <trrenty@gmail.com>
2021-11-16 22:16:05 +0200

    Added more Aspect logging.

--- app/SearchableEncryption/src/main/java/com/example/SearchableEncryption/aop/LoggingAspect.java
+++ app/SearchableEncryption/src/main/java/com/example/SearchableEncryption/aop/LoggingAspect.java
@@ -26,34 +26,52 @@ public class LoggingAspect {
 
     @Pointcut("within(com.example.SearchableEncryption.service..*)" +
             " || within(com.example.SearchableEncryption.controllers..*)" +
-            " || within(com.example.SearchableEncryption.repository..*)" +
             " || within(com.example.SearchableEncryption.security..*)")
     public void applicationPackagePointCut() {
 
     }
 
+    @Pointcut("within(com.example.SearchableEncryption.repository..*)")
+    public void databasePointCut() {
+
+    }
+
     @AfterThrowing(pointcut = "applicationPackagePointCut() && springBeanPointCut()", throwing = "e")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
-        logger.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
+        logger.error("Exception in {} with cause = {}", joinPoint.getSignature().toShortString()
+                , e.getCause() != null ? e.getCause() : "NULL");
+    }
+
+    @Around("databasePointCut()")
+    public void logDatabaseAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Database Access Call in {} with the following params {}."
+                    , joinPoint.getSignature().toShortString()
+                    , Arrays.toString(joinPoint.getArgs())
+            );
+        }
+
+        Object result = joinPoint.proceed();
+        if (logger.isDebugEnabled()) {
+            logger.debug("Database Access End Call in {} with result: {}.", joinPoint.getSignature().toShortString(), result);
+        }
     }
 
     @Around("applicationPackagePointCut() && springBeanPointCut()")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
         if (logger.isDebugEnabled()) {
-            logger.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                    joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+            logger.debug("Enter: {} with argument[s] = {}", joinPoint.getSignature().toShortString(),
+                    Arrays.toString(joinPoint.getArgs()));
         }
         try {
             Object result = joinPoint.proceed();
             if (logger.isDebugEnabled()) {
-                logger.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                        joinPoint.getSignature().getName(), result);
+                logger.debug("Exit: {} with result = {}", joinPoint.getSignature().toShortString(), result);
             }
             return result;
         } catch (IllegalArgumentException e) {
-            logger.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
-                    joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+            logger.error("Illegal argument: {} in {}", Arrays.toString(joinPoint.getArgs()),
+                    joinPoint.getSignature().toShortString());
             throw e;
         }
     }
