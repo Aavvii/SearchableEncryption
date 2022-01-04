59bc00bf5340d757635a0795c3116a70612e2304
croftliss <elisabeta4567@gmail.com>
2022-01-04 15:24:40 +0200

    Deleted old version of API

--- app/SearchableEncryption/src/main/resources/db/migration/v1__AccountTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ACCOUNT (
-    id UUID NOT NULL PRIMARY KEY,
-    firstname VARCHAR(50) NOT NULL,
-    lastname VARCHAR(50) NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-);
